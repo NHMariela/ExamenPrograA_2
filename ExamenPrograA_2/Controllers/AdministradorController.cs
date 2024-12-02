39a800d50575d1a9b4bd750810251cbef1a84c17
Altharian <serranobrendan2@gmail.com>
2024-12-02 14:07:12 -0600

    Add files via upload

---
+++ ExamenPrograA_2/Controllers/AdministradorController.cs
@@ -0,0 +1,21 @@
+﻿using ExamenPrograA_2.Models;
+using ExamenPrograA_2.Models.ViewModels;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ExamenPrograA_2.Controllers
+{
+    [Authorize(Roles = "Administrador")]
+    public class AdminController : Controller
+    {
+        private FincaAgricolaDBEntities db = new FincaAgricolaDBEntities();
+
+        // GET: Admin/Usuarios
+        public ActionResult Usuarios()
+        {
+            var usuarios = db.Usuarios.ToList();
+            return View(usuarios);
+        }
+    }
+}
